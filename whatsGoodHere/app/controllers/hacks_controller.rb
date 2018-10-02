7557546cc01caf5d504d5e5fcb64ae6f5d590f61
Josh Carroccia <agr.carroccia@gmail.com>
2018-10-02 09:17:02 -0400

    added in controllers and started offer build

--- whatsGoodHere/app/controllers/hacks_controller.rb
+++ whatsGoodHere/app/controllers/hacks_controller.rb
@@ -1,2 +1,24 @@
 class HacksController < ApplicationController
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def create
+  end
+
+  private
+
+  def hack_params
+    params.require(:hack).permit(:hack_detail, :profile_id)
+  end
+
 end
--- whatsGoodHere/app/controllers/locations_controller.rb
+++ whatsGoodHere/app/controllers/locations_controller.rb
@@ -1,2 +1,25 @@
 class LocationsController < ApplicationController
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def create
+  end
+
+  private
+
+  def location_params
+    params.require(:location).permit(:location_detail, :profile_id)
+  end
+
+
 end
