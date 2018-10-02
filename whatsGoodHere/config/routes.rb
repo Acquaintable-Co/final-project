f0f5709e70c06e0619b8d7038c5e95f3d8b75553
Geoff <geoff.register@gmail.com>
2018-10-01 20:37:58 -0400

    some sessions controller changes

--- whatsGoodHere/config/routes.rb
+++ whatsGoodHere/config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   
-  resources : profiles
+  get '/login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  resources :profiles
   resources :offers
   resources :locations
   resources :highlights
