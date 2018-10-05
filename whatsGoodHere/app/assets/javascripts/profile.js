b8608a7e2ed7ecd894402ec21ca89aeb90635791
Dan Codae <drcoda@gmail.com>
2018-10-05 12:05:20 -0400

    clean up functionality for hide show

--- whatsGoodHere/app/assets/javascripts/profile.js
+++ whatsGoodHere/app/assets/javascripts/profile.js
@@ -1,22 +1,11 @@
 
-function showOfferDetail () {
-  var offers = document.getElementsByClassName('fare-detail')
-  for (var offer of offers) {
-    if (offer.classList.contains('hidden')) {
-      offer.classList.remove('hidden')
+function showDetail (className) {
+  var divs = document.getElementsByClassName(className)
+  for (var div of divs) {
+    if (div.classList.contains('hidden')) {
+      div.classList.remove('hidden')
     } else {
-      offer.classList.add('hidden')
-    }
-  }
-}
-
-function showHighlightDetail() {
-  var highlights = document.getElementsByClassName('highlight-detail') 
-  for (var highlight of highlights) {
-    if (highlight.classList.contains('hidden')) {
-      highlight.classList.remove('hidden')
-    } else {
-      highlight.classList.add('hidden')
+      div.classList.add('hidden')
     }
   }
 }
