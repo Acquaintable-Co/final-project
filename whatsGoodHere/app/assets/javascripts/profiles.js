83609d07a6d7b526c3c586ac30dc88a0fe1661d7
Dan Codae <drcoda@gmail.com>
2018-10-10 11:19:06 -0400

    add modal functionality for admin user

--- whatsGoodHere/app/assets/javascripts/profiles.js
+++ whatsGoodHere/app/assets/javascripts/profiles.js
@@ -9,3 +9,60 @@ function showDetail (className) {
     }
   }
 }
+
+window.onload = function () {
+  var modal = document.getElementById('fare-modal')
+  var btn = document.getElementById('fare-info')
+  var span = document.getElementsByClassName('close')
+  btn.onclick = function () {
+    console.log('hi')
+    modal.style.display = 'block'
+  }
+  span.onclick = function () {
+    modal.style.display = 'none'
+  }
+
+  window.onclick = function (event) {
+    if (event.target === modal) {
+      modal.style.display = 'none'
+    }
+  }
+}
+
+window.onload = function () {
+  var modal = document.getElementById('tips-modal')
+  var btn = document.getElementById('tips-info')
+  var span = document.getElementsByClassName('close')
+  btn.onclick = function () {
+    console.log('hi')
+    modal.style.display = 'block'
+  }
+  span.onclick = function () {
+    modal.style.display = 'none'
+  }
+
+  window.onclick = function (event) {
+    if (event.target === modal) {
+      modal.style.display = 'none'
+    }
+  }
+}
+
+window.onload = function () {
+  var modal = document.getElementById('specs-modal')
+  var btn = document.getElementById('specs-info')
+  var span = document.getElementsByClassName('close')
+  btn.onclick = function () {
+    console.log('hi')
+    modal.style.display = 'block'
+  }
+  span.onclick = function () {
+    modal.style.display = 'none'
+  }
+
+  window.onclick = function (event) {
+    if (event.target === modal) {
+      modal.style.display = 'none'
+    }
+  }
+}
